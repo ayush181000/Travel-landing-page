2858c921139a81a797dfd6d612f06b17259bbd93
ayush181000 <ayush181000@gmail.com>
2021-10-14 03:22:52 +0530

    Fixed images not showing

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import '../../App.css';
-import image1 from '../../images/img-1.jpg';
 
 export default function Products() {
-  return (
-    <h1 className='products' backgroundimage={image1}>
-      PRODUCTS
-    </h1>
-  );
+  return <h1 className='products'>PRODUCTS</h1>;
 }
