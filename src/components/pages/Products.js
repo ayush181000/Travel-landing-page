3d013b8ec7368f8b4d657e9adabbb5eb3db4cef6
ayush181000 <ayush181000@gmail.com>
2021-10-13 08:35:54 +0530

    Setting up more routes

---
+++ src/components/pages/Products.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../../App.css';
+import image1 from '../../images/img-1.jpg';
+
+export default function Products() {
+  return <h1 className='products'>PRODUCTS</h1>;
+}
